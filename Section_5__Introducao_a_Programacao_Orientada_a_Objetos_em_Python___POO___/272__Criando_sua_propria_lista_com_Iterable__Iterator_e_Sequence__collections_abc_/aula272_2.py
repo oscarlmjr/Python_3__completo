ee9d034f43f2fc3144492866826e0fc75747371d
oscarlmjr <oscarlmjr@gmail.com>
2025-09-09 22:55:14 -0300

    secao_6 274

---
+++ Section_5__Introducao_a_Programacao_Orientada_a_Objetos_em_Python___POO___/272__Criando_sua_propria_lista_com_Iterable__Iterator_e_Sequence__collections_abc_/aula272_2.py
@@ -0,0 +1,21 @@
+# Implementando o protocolo do Iterator em Python
+# Essa é apenas uma aula para introduzir os protocolos de collections.abc no
+# Python. Qualquer outro protocolo poderá ser implementando seguindo a mesma
+# estrutura usada nessa aula.
+# https://docs.python.org/3/library/collections.abc.html
+class MyList:
+    def __init__(self):
+        self._data = {}
+        self._index = 0
+
+    def append(self, value):
+        self._data[self._index] = value
+        self._index += 1
+
+
+if __name__ == '__main__':
+    lista = MyList()
+    lista.append('Maria')
+    lista.append('Luiz')
+    print(lista._data)
+    
---
+++ Section_5__Introducao_a_Programacao_Orientada_a_Objetos_em_Python___POO___/272__Criando_sua_propria_lista_com_Iterable__Iterator_e_Sequence__collections_abc_/aula272_3.py
@@ -0,0 +1,32 @@
+# Implementando o protocolo do Iterator em Python
+# Essa é apenas uma aula para introduzir os protocolos de collections.abc no
+# Python. Qualquer outro protocolo poderá ser implementando seguindo a mesma
+# estrutura usada nessa aula.
+# https://docs.python.org/3/library/collections.abc.html
+from collections.abc import Sequence
+
+
+class MyList(Sequence):
+    def __init__(self):
+        self._data = {}
+        self._index = 0
+
+    def append(self, value):
+        self._data[self._index] = value
+        self._index += 1
+
+    def __len__(self) -> int:
+        return self._index
+
+    def __getitem__(self, index):
+        print('__getitem__')
+        return self._data[index]
+
+
+if __name__ == '__main__':
+    lista = MyList()
+    lista.append('Maria')
+    lista.append('Luiz')
+    print(lista[0])
+    print(len(lista))
+    
