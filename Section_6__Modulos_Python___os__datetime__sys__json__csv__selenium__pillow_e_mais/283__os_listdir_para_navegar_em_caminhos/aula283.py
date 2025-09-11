c73c2d75884ce703501220f9c6d1022a7c4ee79a
oscarlmjr <oscarlmjr@gmail.com>
2025-09-11 17:20:24 -0300

    secao_6 284

---
+++ Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/283__os_listdir_para_navegar_em_caminhos/aula283.py
@@ -0,0 +1,21 @@
+# os.listdir para navegar em caminhos
+# /Users/luizotavio/Desktop/EXEMPLO
+# C:\Users\luizotavio\Desktop\EXEMPLO
+# caminho = r'C:\\Users\\luizotavio\\Desktop\\EXEMPLO'
+import os
+
+
+caminho = os.path.join('D:\\', 'Desenvolvimento', 'Python_3__completo', \
+                       'cursopython_prints', 'Section_6__Modulos_Python_' \
+                    '__os__datetime__sys__json__csv__selenium__pillow_e_mais')
+# caminho = os.path.join('D:\\', 'Saved Games')
+
+for pasta in os.listdir(caminho):
+    caminho_completo_pasta = os.path.join(caminho, pasta)
+    print(pasta)
+
+    if not os.path.isdir(caminho_completo_pasta):
+        continue
+
+    for imagem in os.listdir(caminho_completo_pasta):
+        print('  ', imagem)
---
+++ Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/284__os_walk_para_navegar_de_caminhos_de_forma_recursiva/aula284_3.py
@@ -0,0 +1,22 @@
+# os.walk para navegar de caminhos de forma recursiva
+# os.walk é uma função que permite percorrer uma estrutura de diretórios de
+# maneira recursiva. Ela gera uma sequência de tuplas, onde cada tupla possui
+# três elementos: o diretório atual (root), uma lista de subdiretórios (dirs)
+# e uma lista dos arquivos do diretório atual (files).
+import os
+from itertools import count
+
+
+caminho = os.path.join('D:\\', 'Programs')
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+    print(the_counter, 'Pasta atual', root)
+
+    for dir_ in dirs:
+        print('  ', the_counter, 'Dir:', dir_)
+
+    for file_ in files:
+        print('  ', the_counter, 'FILE:', file_)
+        
