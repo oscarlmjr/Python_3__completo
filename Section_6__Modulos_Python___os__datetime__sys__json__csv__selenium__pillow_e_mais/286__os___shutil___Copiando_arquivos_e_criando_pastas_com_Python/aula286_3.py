6e5bd635a0c53c727d0e8db2264179832d398487
oscarlmjr <oscarlmjr@gmail.com>
2025-09-12 21:58:01 -0300

    secao_6 290

--- Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/286__os___shutil___Copiando_arquivos_e_criando_pastas_com_Python/aula286_3.py
+++ Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/286__os___shutil___Copiando_arquivos_e_criando_pastas_com_Python/aula286_3.py
@@ -9,6 +9,7 @@ HOME = os.path.expanduser('~')
 DESKTOP = os.path.join(HOME, 'Desktop')
 PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
 NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+print(NOVA_PASTA)
 
 os.makedirs(NOVA_PASTA, exist_ok=True)
 
---
+++ Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/289__json_dumps_e_json_loads_com_strings___typing_TypedDict/aula289.py
@@ -0,0 +1,29 @@
+# json.dumps e json.loads com strings + typing.TypedDict
+# Ao converter de Python para JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
+import json
+from pprint import pprint
+
+
+string_json = '''
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": true,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": null
+}
+'''
+
+filme = json.loads(string_json)
+pprint(filme, width=40)
+print(filme['title'])
