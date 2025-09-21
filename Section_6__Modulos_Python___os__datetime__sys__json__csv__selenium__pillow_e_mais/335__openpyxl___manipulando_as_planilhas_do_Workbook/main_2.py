2508211e946e32dafaa2672333fd9d6455b55ea4
oscarlmjr <oscarlmjr@gmail.com>
2025-09-20 21:21:50 -0300

    secao_6 aula335

---
+++ Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/335__openpyxl___manipulando_as_planilhas_do_Workbook/main_2.py
@@ -0,0 +1,42 @@
+# openpyxl - manipulando as planilhas do Workbook
+# Com essa biblioteca será possível ler e escrever dados em células
+# específicas, formatar células, inserir gráficos,
+# criar fórmulas, adicionar imagens e outros elementos gráficos às suas
+# planilhas. Ela é útil para automatizar tarefas envolvendo planilhas do
+# Excel, como a criação de relatórios e análise de dados e/ou facilitando a
+# manipulação de grandes quantidades de informações.
+# Instalação necessária: pip install openpyxl
+# Documentação: https://openpyxl.readthedocs.io/en/stable/
+from pathlib import Path
+
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+ROOT_FOLDER = Path(__file__).parent.parent
+WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
+
+workbook = Workbook()
+
+sheet_name = 'Minha planilha'
+workbook.create_sheet(sheet_name, 0)
+worksheet: Worksheet = workbook.active
+
+print(workbook.sheetnames)
+
+worksheet.cell(1, 1, 'Nome')
+worksheet.cell(1, 2, 'Idade')
+worksheet.cell(1, 3, 'Nota')
+
+students = [
+    # nome      idade nota
+    ['João',    14,   5.5],
+    ['Maria',   13,   9.7],
+    ['Luiz',    15,   8.8],
+    ['Alberto', 16,   10],
+]
+
+for student in students:
+    worksheet.append(student)
+
+workbook.save(WORKBOOK_PATH)
+
