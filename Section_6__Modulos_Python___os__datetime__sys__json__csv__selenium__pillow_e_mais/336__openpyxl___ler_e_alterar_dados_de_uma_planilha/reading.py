7477726639fdf1eda015b0448a0701f60c23dbb2
oscarlmjr <oscarlmjr@gmail.com>
2025-09-21 21:40:40 -0300

    secao_7 aula341

--- Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/336__openpyxl___ler_e_alterar_dados_de_uma_planilha/reading.py
+++ Section_6__Modulos_Python___os__datetime__sys__json__csv__selenium__pillow_e_mais/336__openpyxl___ler_e_alterar_dados_de_uma_planilha/reading.py
@@ -8,32 +8,34 @@
 # Instalação necessária: pip install openpyxl
 # Documentação: https://openpyxl.readthedocs.io/en/stable/
 from pathlib import Path
-
 from openpyxl import Workbook, load_workbook
 from openpyxl.cell import Cell
 from openpyxl.worksheet.worksheet import Worksheet
 
-ROOT_FOLDER = Path(__file__).parent
+
+ROOT_FOLDER = Path(__file__).parent.parent
 WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
 
 # Carregando um arquivo do excel
 workbook: Workbook = load_workbook(WORKBOOK_PATH)
-
 # Nome para a planilha
 sheet_name = 'Minha planilha'
-
 # Selecionou a planilha
 worksheet: Worksheet = workbook[sheet_name]
 
 row: tuple[Cell]
 for row in worksheet.iter_rows(min_row=2):
-    for cell in row:
-        print(cell.value, end='\t')
+	for cell in row:
+		print(cell.value, end='\t')
 
-        if cell.value == 'Maria':
-            worksheet.cell(cell.row, 2, 23)
-    print()
+# print()
+# print(worksheet)
+# print(worksheet['B3'])
+# print(worksheet['B3'].value)
+		if cell.value == 'Maria':
+			worksheet.cell(cell.row, 2, 23)
+	print()
 
-# worksheet['B3'].value = 14
+worksheet['B3'].value = 14
 
 workbook.save(WORKBOOK_PATH)
