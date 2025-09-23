0683715cc9110b00cf7d3bf826d8c2ec85b6edc1
oscarlmjr <oscarlmjr@gmail.com>
2025-09-23 18:31:24 -0300

    secao_7 aula353

--- Section_7__PySide6___Interface_grafica_corn_QT_6_no_Python___GUI_para_Desktop_/354__Calculadora__criando_um_botao_com_QPushButton_no_PySide6/main.py
+++ Section_7__PySide6___Interface_grafica_corn_QT_6_no_Python___GUI_para_Desktop_/354__Calculadora__criando_um_botao_com_QPushButton_no_PySide6/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from buttons import ButtonsGrid
+from buttons import Button
 from display import Display
 from info import Info
 from main_window import MainWindow
@@ -8,30 +8,32 @@ from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from styles import setupTheme
 from variables import WINDOW_ICON_PATH
- 
+
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
-    setupTheme(app)
+    setupTheme()
     window = MainWindow()
- 
+
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
- 
+
     # Info
-    info = Info('Sua conta')
-    window.addWidgetToVLayout(info)
- 
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
+
     # Display
     display = Display()
-    window.addWidgetToVLayout(display)
- 
-    # Grid
-    buttonsGrid = ButtonsGrid(display, info, window)
-    window.vLayout.addLayout(buttonsGrid)
- 
+    window.addToVLayout(display)
+
+    button = Button('Texto do botão')
+    window.addToVLayout(button)
+
+    button2 = Button('Texto do botão')
+    window.addToVLayout(button2)
+
     # Executa tudo
     window.adjustFixedSize()
     window.show()
