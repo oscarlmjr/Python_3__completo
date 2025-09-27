c74ee96677767b28610fe63a60e1262721b808ba
oscarlmjr <oscarlmjr@gmail.com>
2025-09-26 21:30:29 -0300

    secao_7 aula_366

--- Section_7__PySide6___Interface_grafica_corn_QT_6_no_Python___GUI_para_Desktop_/368__Calculadora__capturando_teclas_ENTER__Backspace_e_ESC/display.py
+++ Section_7__PySide6___Interface_grafica_corn_QT_6_no_Python___GUI_para_Desktop_/368__Calculadora__capturando_teclas_ENTER__Backspace_e_ESC/display.py
@@ -1,12 +1,16 @@
-# from PySide6.QtCore import Qt
 from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
+from utils import isEmpty
 from variables import BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
 
 
 class Display(QLineEdit):
 	eqRequested = Signal()
+
+	# eqPressed = Signal()
+	# delPressed = Signal()
+	# clearPressed = Signal()
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
@@ -21,18 +25,37 @@ class Display(QLineEdit):
 		self.setTextMargins(*margins)
 
 	def keyPressEvent(self, event: QKeyEvent) -> None:
-		# print(event.text())
-		# print(event.key())
+		# text = event.text().strip()
 		key = event.key()
 		KEYS = Qt.Key
 
 		isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
 
-		# if key == '':
+		# isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete]
+		# isEsc = key in [KEYS.Key_Escape]
+
 		if isEnter:
 			print('Enter pressionado, sinal emitido', type(self).__name__)
 			self.eqRequested.emit()
-			return event.ignore()
 
+			# self.eqPressed.emit()
+
+			return event.ignore()
 		# return super().keyPressEvent(event)
+
+		# if isDelete:
+		# 	print('isDelete pressionado, sinal emitido', type(self).__name__)
+		# 	self.delPressed.emit()
+		# 	return event.ignore()
+
+		# if isEsc:
+		# 	print('isEsc pressionado, sinal emitido', type(self).__name__)
+		# 	self.clearPressed.emit()
+		# 	return event.ignore()
+
+		# # Não passar daqui se não tiver texto
+		# if isEmpty(text):
+		# 	return event.ignore()
+
+		# print('Texto', text)
 
