95663c036150d5ba5407683c306ee78263c286e3
oscarlmjr <oscarlmjr@gmail.com>
2025-09-28 12:26:27 -0300

    secao_7 aula375

---
+++ Section_7__PySide6___Interface_grafica_corn_QT_6_no_Python___GUI_para_Desktop_/375__Aula_extra__opcional___empacotando_a_calculadora_com_PyInstaller__Geral_/aula375.py
@@ -0,0 +1,4 @@
+# Documentação do PyInstaller:
+# https://pyinstaller.org/en/stable/
+# Tutorial do Python GUIS:
+# https://www.pythonguis.com/tutorials/packaging-pyside6-applications-windows-pyinstaller-installforge/
