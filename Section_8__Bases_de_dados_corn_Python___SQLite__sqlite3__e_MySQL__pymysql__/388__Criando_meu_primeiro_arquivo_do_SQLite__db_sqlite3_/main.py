4c2e81d6bbdba2c52eb0da8af5fe5a0c1c8b1f16
oscarlmjr <oscarlmjr@gmail.com>
2025-10-02 11:43:16 -0300

    secao_8 aula391

---
+++ Section_8__Bases_de_dados_corn_Python___SQLite__sqlite3__e_MySQL__pymysql__/388__Criando_meu_primeiro_arquivo_do_SQLite__db_sqlite3_/main.py
@@ -0,0 +1,15 @@
+# https://www.sqlite.org/doclist.html
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# SQL
+
+cursor.close()
+connection.close()
---
+++ Section_8__Bases_de_dados_corn_Python___SQLite__sqlite3__e_MySQL__pymysql__/391__Usando_placeholders_para_maior_seguranca__bindings__no_SQLite/main.py
@@ -0,0 +1,49 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# CUIDADO: fazendo delete sem where
+cursor.execute(
+	f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+	f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+# Cria a tabela
+cursor.execute(
+	f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+	'('
+	'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+	'name TEXT,'
+	'weight REAL'
+	')'
+)
+connection.commit()
+
+# Registrar valores nas colunas da tabela
+# CUIDADO: sql injection
+cursor.execute(
+	# sql = (
+	f'INSERT INTO {TABLE_NAME} '
+	'(id, name, weight) '
+	# '(name, weight) '
+	'VALUES '
+	# '(NULL, "Luiz Otávio", 9.9)'
+	'(NULL, "Helena", 4), (NULL, "Eduardo", 10)'
+	# '(?, ?)'
+)
+# cursor.execute(sql, ['Joana', 4])
+connection.commit()
+# print(sql)
+
+cursor.close()
+connection.close()
