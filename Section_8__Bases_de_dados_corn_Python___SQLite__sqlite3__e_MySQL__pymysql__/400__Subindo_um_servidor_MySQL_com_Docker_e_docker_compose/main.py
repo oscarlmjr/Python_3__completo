c44b7fff67e6cd93d230c42e3b0af14add2b2874
oscarlmjr <oscarlmjr@gmail.com>
2025-10-07 21:48:33 -0300

    secao_8 aula405

--- Section_8__Bases_de_dados_corn_Python___SQLite__sqlite3__e_MySQL__pymysql__/400__Subindo_um_servidor_MySQL_com_Docker_e_docker_compose/main.py
+++ Section_8__Bases_de_dados_corn_Python___SQLite__sqlite3__e_MySQL__pymysql__/400__Subindo_um_servidor_MySQL_com_Docker_e_docker_compose/main.py
@@ -2,26 +2,53 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
-# import os
+import os
 
-# import dotenv
+import dotenv
 import pymysql
 
-# dotenv.load_dotenv()
+# TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
 
 connection = pymysql.connect(
-    host='localhost',
-    user='usuario',
-    password='senha',
-    database='base_de_dados',
-	# host=os.environ['MYSQL_HOST'],
-    # user=os.environ['MYSQL_USER'],
-    # password=os.environ['MYSQL_PASSWORD'],
-    # database=os.environ['MYSQL_DATABASE'],
+	host=os.environ['MYSQL_HOST'],
+	user=os.environ['MYSQL_USER'],
+	password=os.environ['MYSQL_PASSWORD'],
+	database=os.environ['MYSQL_DATABASE'],
+	# charset='utf8mb4'
 )
-# connection.close()
 
 with connection:
-    with connection.cursor() as cursor:
-        # SQL
-        print(cursor)
+	with connection.cursor() as cursor:
+		cursor.execute(
+			'CREATE TABLE IF NOT EXISTS customers ('
+			# f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+			'id INT NOT NULL AUTO_INCREMENT, '
+			'nome VARCHAR(50) NOT NULL, '
+			'idade INT NOT NULL, '
+			'PRIMARY KEY (id)'
+			') '
+		)
+		print(cursor)
+		# CUIDADO: ISSO LIMPA A TABELA
+		# cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # type: ignore
+		# connection.commit()
+
+		# # Começo a manipular dados a partir daqui
+
+		# with connection.cursor() as cursor:
+		# 	cursor.execute(  # type: ignore
+		# 		f'INSERT INTO {TABLE_NAME} '
+		# 		'(nome, idade) VALUES ("Luiz", 25) '
+		# 	)
+		# 	cursor.execute(  # type: ignore
+		# 		f'INSERT INTO {TABLE_NAME} '
+		# 		'(nome, idade) VALUES ("Luiz", 25) '
+		# 	)
+		# 	result = cursor.execute(  # type: ignore
+		# 		f'INSERT INTO {TABLE_NAME} '
+		# 		'(nome, idade) VALUES ("Luiz", 25) '
+		# 	)
+		# 	print(result)
+		# connection.commit()
